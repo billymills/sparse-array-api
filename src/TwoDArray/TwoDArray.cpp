6ee981f8f9f1ee94b69ba64abdc03aa5807d985a
Billy Mills <wmills@unca.edu>
2012-10-21 19:19:53 -0400

    array being initialized, access working

--- src/TwoDArray/TwoDArray.cpp
+++ src/TwoDArray/TwoDArray.cpp
@@ -12,18 +12,40 @@ using std::endl;
 
 //constructor
 template <typename T>
-TwoDArray<T>::TwoDArray(int r, int c, T){
-	T** myArray = new T*[c];
+TwoDArray<T>::TwoDArray(int r, int c, T def){
+	//default = def;
+	//cout << "in const" << endl;
+	row = r;
+	column = c;
+	//create 2D array
+	myArray = new T*[c];
 	for(int i=0; i<c; ++i){
 		myArray[i] = new T[r];
 	}
-	//next need to initialize array to default value;	
+	//cout << "array created" << endl;
+	//next need to initialize array to default value;
+	for(int j=0; j<c; ++j){
+		//cout << "in initializer" << endl;
+		for(int k=0; k<r; ++k){
+			//cout << "in inner for" << endl;
+			myArray[j][k] = def;
+		}
+	}	
 }
 
 //destructor
 template <typename T>
 TwoDArray<T>::~TwoDArray(){
+	for(int i=0;i<row;++i){
+		delete[] myArray[i];
+	}
+	delete[] myArray;
+}
 
+//return value a given indices
+template <typename T>
+T TwoDArray<T>::access(int r, int c){
+	return myArray[r][c];
 }
 
 //insert the value v at index r, c

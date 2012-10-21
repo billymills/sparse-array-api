b6879051542f47658686e85a380e36baa56c4add
Billy Mills <wmills@unca.edu>
2012-10-21 19:20:46 -0400

    array being initialized, access working

--- src/testing/array_test.cpp
+++ src/testing/array_test.cpp
@@ -4,6 +4,7 @@
 //date:		Fall 2012
 
 #include <iostream>
+#include "../TwoDArray/TwoDArray.h"
 
 using std::cout;
 using std::endl;
@@ -11,5 +12,16 @@ using std::endl;
 int main(){
 
 	cout << "hello world" << endl;
+	TwoDArray<int>* newArray = new TwoDArray<int>(10, 10, 0);
+	
+	cout << "array created" << endl;
+
+	cout << newArray->access(1,1) << endl;
+	delete newArray;	
+
+
+	TwoDArray<int>* nextArray = new TwoDArray<int>(5,5,1);
+	cout << nextArray->access(3,3) << endl;
+	delete nextArray;
 
 }
